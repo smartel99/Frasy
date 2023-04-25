3ad0ca1621afc928839508ed17c528981c3b0cdb
DoubleNom <ptdn.work@gmail.com>
2023-04-25 16:19:02 -0400

    Removed unnecessary debug log telling which command is being run

--- Frasy/src/utils/lua/orchestrator/orchestrator.cpp
+++ Frasy/src/utils/lua/orchestrator/orchestrator.cpp
@@ -156,7 +156,6 @@ void Orchestrator::LoadIb(sol::state& lua)
     BR_LUA_DEBUG("Loading commands");
     for (const auto& [id, fun] : device.GetCommands())
     {
-        BR_LUA_DEBUG("Command {}", fun.Name);
         if (isExecution) { LoadIbCommandForExecution(lua, fun); }
         else { LoadIbCommandForValidation(lua, fun); }
     }
