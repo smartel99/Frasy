b62249212e08a12035d738201aed18d66b1e521d
Paul Thomas <paulthomas.dn@gmail.com>
2024-07-03 14:49:18 -0400

    Image support for lua popup

--- Frasy/src/utils/lua/popup.h
+++ Frasy/src/utils/lua/popup.h
@@ -18,47 +18,54 @@
 #define COPY_LUA_PY_FRASY_SRC_UTILS_LUA_POPUP_H
 
 #include "Brigerad.h"
+#include "Brigerad/Core/File.h"
 
 #include <functional>
 #include <shared_mutex>
 #include <sol/sol.hpp>
 #include <string>
 
-namespace Frasy::Lua
-{
+namespace Frasy::Lua {
 
-class Popup
-{
+class Popup {
 public:
-    struct Element
-    {
-        enum class Kind
-        {
+    struct Element {
+        enum class Kind {
             Text,
             Input,
             Button,
+            Image,
         } kind;
         std::string text;
-        explicit Element(Kind kind, const std::string& text) : kind(kind), text(text) {}
+        explicit    Element(Kind kind, const std::string& text) : kind(kind), text(text) {}
+        virtual ~   Element() = default;
     };
 
-    struct Text : Element
-    {
-        Text(const std::string& text) : Element(Kind::Text, text) {}
+    struct Text : Element {
+         Text(const std::string& text) : Element(Kind::Text, text) {}
     };
 
-    struct Input : Element
-    {
-        Input(const std::string& text, std::size_t index) : Element(Kind::Input, text), index(index) {}
+    struct Input : Element {
+         Input(const std::string& text, std::size_t index) : Element(Kind::Input, text), index(index) {}
         static constexpr std::size_t vBufLen       = 50;
         char                         vBuf[vBufLen] = "";
         std::size_t                  index         = 0;
     };
 
-    struct Button : Element
-    {
-        Button(const std::string& text, sol::function action) : Element(Kind::Button, text), action(action) {}
+    struct Button : Element {
+         Button(const std::string& text, sol::function action) : Element(Kind::Button, text), action(action) {}
         sol::function action;
+    };
+
+    struct Image : Element {
+        Image(const std::string& path, std::size_t width, std::size_t height)
+        : Element(Kind::Image, path), width(width), height(height)
+        {
+        }
+
+        std::size_t                        width  = 0;
+        std::size_t                        height = 0;
+        Brigerad::Ref<Brigerad::Texture2D> texture;
     };
 
 private:
@@ -70,8 +77,8 @@ private:
     std::shared_mutex                     m_luaMutex;
 
 public:
-    Popup() = default;
-    explicit Popup(std::size_t uut, sol::table builder);
+                                    Popup() = default;
+    explicit                        Popup(std::size_t uut, sol::table builder);
     static std::string              GetName(std::size_t uut, sol::table builder);
     const std::string&              GetName() { return m_name; }
     const std::vector<std::string>& GetInputs() { return m_inputs; }
