029882c30a7b8091919c0ef1ec2b87640da3f526
Paul Thomas <paulthomas.dn@gmail.com>
2024-07-08 17:30:32 -0400

    Fix number utility, the return
    File was not saved, great...

--- Frasy/lua/utils/is_unsigned/is_unsigned_16.lua
+++ Frasy/lua/utils/is_unsigned/is_unsigned_16.lua
@@ -1,1 +1,4 @@
-return function(value) return type(value) == "number" and (value // 1) == value and 0 <= value and value <= 65535
+return function(value)
+    return type(value) == "number" and (value // 1) == value and 0 <= value and
+               value <= 65535
+end
