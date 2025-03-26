ab488030e32f96f3bfccaa700481e1a4d457170b
Paul Thomas <paulthomas.dn@gmail.com>
2025-03-26 15:09:24 -0400

    Update script to use provided hashdir.exe

--- demo_mode/generate_hashes.bat
+++ demo_mode/generate_hashes.bat
@@ -5,5 +5,5 @@ set lua_core_dir=%~dp0..\Frasy\lua\core
 set lua_core_hash=%~dp0..\Frasy\lua\core\hash
 
 @echo on
-hashdir.exe %lua_user_dir% %* > %lua_user_hash%
-hashdir.exe %lua_core_dir% -f in f .+\.lua > %lua_core_hash%
+%~dp0..\scripts\Windows\hashdir.exe %lua_user_dir% %* > %lua_user_hash%
+%~dp0..\scripts\Windows\hashdir.exe %lua_core_dir% -f in f .+\.lua > %lua_core_hash%
