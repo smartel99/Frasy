b0ac7690cce04f4de7de9dc08403d6efa373a801
Paul Thomas <paulthomas.dn@gmail.com>
2025-11-25 10:57:27 -0500

    Update expectations to allow nil values
    Boolean and Number expectations now allows nil values, which produce a test failure rather than a test crash.

--- Frasy/lua/core/framework/expectation/common.lua
+++ Frasy/lua/core/framework/expectation/common.lua
@@ -1,54 +1,54 @@
 function ExpectToBeTrue(value)
-    if Context.info.stage ~= Stage.execution then return {pass = true} end
+    if Context.info.stage ~= Stage.execution then return { pass = true } end
     local result = {}
     result.method = "ToBeTrue"
     result.expected = true
-    result.pass = value == result.expected
+    result.pass = type(value) == "boolean" and value == result.expected or false
     return result
 end
 
 function ExpectToBeFalse(value)
-    if Context.info.stage ~= Stage.execution then return {pass = true} end
+    if Context.info.stage ~= Stage.execution then return { pass = true } end
     local result = {}
     result.method = "ToBeFalse"
     result.expected = false
-    result.pass = value == result.expected
+    result.pass = type(value) == "boolean" and value == result.expected or false
     return result
 end
 
 function ExpectToBeEqual(value, expected)
-    if Context.info.stage ~= Stage.execution then return {pass = true} end
+    if Context.info.stage ~= Stage.execution then return { pass = true } end
     local result = {}
     result.method = "ToBeEqual"
     result.expected = expected
-    result.pass = value == result.expected
+    result.pass = type(value) == type(expected) and value == result.expected
     return result
 end
 
 function ExpectToBeNear(value, expected, deviation)
-    if Context.info.stage ~= Stage.execution then return {pass = true} end
+    if Context.info.stage ~= Stage.execution then return { pass = true } end
     local result = {}
     result.method = "ToBeNear"
     result.expected = expected
     result.deviation = math.abs(deviation)
     result.min = expected - deviation
     result.max = expected + deviation
-    result.pass = result.min <= value and value <= result.max
+    result.pass = type(value) == "number" and (result.min <= value and value <= result.max) or false
     return result
 end
 
 function ExpectToBeInRange(value, min, max)
-    if Context.info.stage ~= Stage.execution then return {pass = true} end
+    if Context.info.stage ~= Stage.execution then return { pass = true } end
     local result = {}
     result.method = "ToBeInRange"
     result.min = min
     result.max = max
-    result.pass = min <= value and value <= max
+    result.pass = type(value) == "number" and (min <= value and value <= max) or false
     return result
 end
 
 function ExpectToBeInPercentage(value, expected, percentage)
-    if Context.info.stage ~= Stage.execution then return {pass = true} end
+    if Context.info.stage ~= Stage.execution then return { pass = true } end
     local result = {}
     result.method = "ToBeInPercentage"
     result.expected = expected
@@ -56,48 +56,48 @@ function ExpectToBeInPercentage(value, expected, percentage)
     result.deviation = math.abs(expected * percentage / 100)
     result.min = expected - result.deviation
     result.max = expected + result.deviation
-    result.pass = result.min <= value and value <= result.max
+    result.pass = type(value) == "number" and (result.min <= value and value <= result.max) or false
     return result
 end
 
 function ExpectToBeGreater(value, min)
-    if Context.info.stage ~= Stage.execution then return {pass = true} end
+    if Context.info.stage ~= Stage.execution then return { pass = true } end
     local result = {}
     result.method = "ToBeGreater"
     result.min = min
-    result.pass = value > min
+    result.pass = type(value) == "number" and value > min or false
     return result
 end
 
 function ExpectToBeGreaterOrEqual(value, min)
-    if Context.info.stage ~= Stage.execution then return {pass = true} end
+    if Context.info.stage ~= Stage.execution then return { pass = true } end
     local result = {}
     result.method = "ToBeGreaterOrEqual"
     result.min = min
-    result.pass = value >= min
+    result.pass = type(value) == "number" and value >= min or false
     return result
 end
 
 function ExpectToBeLesser(value, max)
-    if Context.info.stage ~= Stage.execution then return {pass = true} end
+    if Context.info.stage ~= Stage.execution then return { pass = true } end
     local result = {}
     result.method = "ToBeLesser"
     result.max = max
-    result.pass = value < max
+    result.pass = type(value) == "number" and value < max or false
     return result
 end
 
 function ExpectToBeLesserOrEqual(value, max)
-    if Context.info.stage ~= Stage.execution then return {pass = true} end
+    if Context.info.stage ~= Stage.execution then return { pass = true } end
     local result = {}
     result.method = "ToBeLesserOrEqual"
     result.max = max
-    result.pass = value <= max
+    result.pass = type(value) == "number" and value <= max or false
     return result
 end
 
 function ExpectToBeType(value, expected)
-    if Context.info.stage ~= Stage.execution then return {pass = true} end
+    if Context.info.stage ~= Stage.execution then return { pass = true } end
     local result = {}
     result.method = "ToBeType"
     result.expected = expected
@@ -107,11 +107,10 @@ function ExpectToBeType(value, expected)
 end
 
 function ExpectToBeMatch(value, pattern)
-    if Context.info.stage ~= Stage.execution then return {pass = true} end
+    if Context.info.stage ~= Stage.execution then return { pass = true } end
     local result = {}
     result.method = "ToMatch"
     result.pattern = pattern
-    result.pass = false
-    for _ in string.gmatch(value, pattern) do result.pass = true end
+    result.pass = type(value) == "string" and string.match(value, pattern) ~= nil or false
     return result
 end
