675f62e8c0ddccb998baf8f4751cef5c6466d595
Sam Martel <martelsamuel00@gmail.com>
2026-02-15 19:20:00 -0500

    Catches (most) exceptions (but only on the main thread)

--- Brigerad/src/Brigerad/Core/EntryPoint.h
+++ Brigerad/src/Brigerad/Core/EntryPoint.h
@@ -10,16 +10,20 @@
 #include <string>
 #include <print>
 #include <fstream>
-
-#include <signal.h>
+#include <format>
+#include <cstddef>
 
 #if defined(BR_PLATFORM_WINDOWS) || defined(BR_PLATFORM_LINUX)
 
 extern Brigerad::Application* Brigerad::CreateApplication(int argc, char** argv);
 
 namespace Brigerad::Internals_do_not_use::Details {
-inline std::string   crashReportHeader;
-inline std::ofstream crashReportFile;
+static constexpr std::size_t      s_crashReportHeaderMaxSize = 1024;
+static constexpr std::string_view s_stacktraceHeader         = "\nStack trace (most recent call first):\n";
+static constexpr std::size_t      s_crashReportContentSize   = s_crashReportHeaderMaxSize - s_stacktraceHeader.size();
+inline std::string                crashReportContent;
+inline std::string                crashReportHeader;
+inline std::ofstream              crashReportFile;
 
 inline int test(int x, int y)
 {
@@ -34,22 +38,113 @@ inline void test2()
 
 inline void printException(const cpptrace::stacktrace& trace)
 {
+    crashReportContent.shrink_to_fit();
+    auto res = std::format_to_n(crashReportHeader.begin(),
+                                s_crashReportHeaderMaxSize,
+                                "{}{}",
+                                crashReportContent.c_str(),
+                                s_stacktraceHeader);
+    crashReportHeader.resize(res.size);
     auto formatter = cpptrace::formatter{}
-                     // TODO Add header to show crashReportHeader + "Stack trace (most recent call first):"
+                     .header(std::move(crashReportHeader))
                      .hide_exception_machinery(true)
                      .symbols(cpptrace::formatter::symbol_mode::pretty)
                      .snippets(true);
 
     // TODO Print to file and to console
-
+    formatter.print(trace);
+    formatter.print(crashReportFile, trace);
 }
 
-inline int divide_zero_filter(int code)
+inline int parse_filter(unsigned long code)
 {
-    if (code == STATUS_INTEGER_DIVIDE_BY_ZERO || code == EXCEPTION_FLT_DIVIDE_BY_ZERO) {
-        return EXCEPTION_EXECUTE_HANDLER;
+    switch (code) {
+        case EXCEPTION_ACCESS_VIOLATION: std::format_to_n(crashReportContent.begin(),
+                                                          s_crashReportContentSize,
+                                                          "Access Violation");
+            return EXCEPTION_EXECUTE_HANDLER;
+        case EXCEPTION_ARRAY_BOUNDS_EXCEEDED: std::format_to_n(crashReportContent.begin(),
+                                                               s_crashReportContentSize,
+                                                               "Array Bounds Exceeded");
+            return EXCEPTION_EXECUTE_HANDLER;
+        case EXCEPTION_BREAKPOINT: std::format_to_n(crashReportContent.begin(),
+                                                    s_crashReportContentSize,
+                                                    "Breakpoint Hit");
+            return EXCEPTION_EXECUTE_HANDLER;
+        case EXCEPTION_DATATYPE_MISALIGNMENT: std::format_to_n(crashReportContent.begin(),
+                                                               s_crashReportContentSize,
+                                                               "Datatype Misalignment");
+            return EXCEPTION_EXECUTE_HANDLER;
+        case EXCEPTION_FLT_DENORMAL_OPERAND: std::format_to_n(crashReportContent.begin(),
+                                                              s_crashReportContentSize,
+                                                              "Floating Point Denormal Operand");
+            return EXCEPTION_EXECUTE_HANDLER;
+        case EXCEPTION_FLT_DIVIDE_BY_ZERO: std::format_to_n(crashReportContent.begin(),
+                                                            s_crashReportContentSize,
+                                                            "Floating Point Divide by Zero");
+            return EXCEPTION_EXECUTE_HANDLER;
+        case EXCEPTION_FLT_INEXACT_RESULT: std::format_to_n(crashReportContent.begin(),
+                                                            s_crashReportContentSize,
+                                                            "Floating Point Inexact Result");
+            return EXCEPTION_EXECUTE_HANDLER;
+        case EXCEPTION_FLT_INVALID_OPERATION: std::format_to_n(crashReportContent.begin(),
+                                                               s_crashReportContentSize,
+                                                               "Floating Point Invalid Operation");
+            return EXCEPTION_EXECUTE_HANDLER;
+        case EXCEPTION_FLT_OVERFLOW: std::format_to_n(crashReportContent.begin(),
+                                                      s_crashReportContentSize,
+                                                      "Floating Point Overflow");
+            return EXCEPTION_EXECUTE_HANDLER;
+        case EXCEPTION_FLT_STACK_CHECK: std::format_to_n(crashReportContent.begin(),
+                                                         s_crashReportContentSize,
+                                                         "The stack overflowed or underflowed as the result of a floating-point operation.");
+            return EXCEPTION_EXECUTE_HANDLER;
+        case EXCEPTION_FLT_UNDERFLOW: std::format_to_n(crashReportContent.begin(),
+                                                       s_crashReportContentSize,
+                                                       "The exponent of a floating-point operation is less than the magnitude allowed by the corresponding type.");
+            return EXCEPTION_EXECUTE_HANDLER;
+        case EXCEPTION_ILLEGAL_INSTRUCTION: std::format_to_n(crashReportContent.begin(),
+                                                             s_crashReportContentSize,
+                                                             "Illegal Instruction");
+            return EXCEPTION_EXECUTE_HANDLER;
+        case EXCEPTION_IN_PAGE_ERROR: std::format_to_n(crashReportContent.begin(),
+                                                       s_crashReportContentSize,
+                                                       "Thread tried accessing a page that was not present, and the system was unable to load the page.");
+            return EXCEPTION_EXECUTE_HANDLER;
+        case EXCEPTION_INT_DIVIDE_BY_ZERO: std::format_to_n(crashReportContent.begin(),
+                                                            s_crashReportContentSize,
+                                                            "Integer Division by Zero");
+            return EXCEPTION_EXECUTE_HANDLER;
+        case EXCEPTION_INT_OVERFLOW: std::format_to_n(crashReportContent.begin(),
+                                                      s_crashReportContentSize,
+                                                      "The result of an integer operation cause a carry out of the most significant bit of the result");
+            return EXCEPTION_EXECUTE_HANDLER;
+        case EXCEPTION_INVALID_DISPOSITION: std::format_to_n(crashReportContent.begin(),
+                                                             s_crashReportContentSize,
+                                                             "EXCEPTION_INVALID_DISPOSITION (If you see this, good luck lmao)");
+            return EXCEPTION_EXECUTE_HANDLER;
+        case EXCEPTION_NONCONTINUABLE_EXCEPTION: std::format_to_n(crashReportContent.begin(),
+                                                                  s_crashReportContentSize,
+                                                                  "Thread tried to continue execution after a noncontinuable exception occurred.");
+            return EXCEPTION_EXECUTE_HANDLER;
+        case EXCEPTION_PRIV_INSTRUCTION: std::format_to_n(crashReportContent.begin(),
+                                                          s_crashReportContentSize,
+                                                          "Thread tried to execute an instruction whose operation is not allowed in the current machine mode.");
+            return EXCEPTION_EXECUTE_HANDLER;
+        case EXCEPTION_SINGLE_STEP: std::format_to_n(crashReportContent.begin(),
+                                                     s_crashReportContentSize,
+                                                     "A trace trap or other single-instruction mechanism signaled that one instruction has been executed. (If you see this, wtf?)");
+            return EXCEPTION_EXECUTE_HANDLER;
+        case EXCEPTION_STACK_OVERFLOW: std::format_to_n(crashReportContent.begin(),
+                                                        s_crashReportContentSize,
+                                                        "The thread used up its stack");
+            return EXCEPTION_EXECUTE_HANDLER;
+        default: std::format_to_n(crashReportContent.begin(),
+                                  s_crashReportContentSize,
+                                  "Unknown Exception: 0x{:x}",
+                                  code);
+            return EXCEPTION_EXECUTE_HANDLER;
     }
-    return EXCEPTION_CONTINUE_SEARCH;
 }
 }
 
@@ -67,10 +162,12 @@ int main(int argc, char** argv)
                     crashReportFile = std::ofstream{"crash_report.txt"};
                     BR_CORE_ASSERT(crashReportFile.is_open(), "Unable to open crash report file!");
                     // Reserve memory for the string so that we are guaranteed to have the space for it, even in the case we fucked the dog way too hard
+                    crashReportHeader.resize(s_crashReportHeaderMaxSize, 0);
+                    crashReportContent.resize(s_crashReportContentSize, 0);
 
                     std::string path = std::filesystem::current_path().string();
                     //    BR_CORE_WARN("Running from: {0}", path);
-                    test(10, 0);
+                    // test(10, 0);
                     // test2();
 
                     BR_PROFILE_END_SESSION();
@@ -82,16 +179,16 @@ int main(int argc, char** argv)
                     BR_PROFILE_END_SESSION();
                 }
             CPPTRACE_CATCH(std::exception& e) {
-                // TODO Save the exception's what in the header string
-                std::cerr << "Unhandled exception occurred: " << e.what() << std::endl;
+                std::format_to_n(crashReportContent.begin(),
+                                 s_crashReportContentSize,
+                                 "Unhandled exception: {}",
+                                 e.what());
                 printException(cpptrace::from_current_exception());
             }
         }();
     }
-    CPPTRACE_SEH_EXCEPT(divide_zero_filter(GetExceptionCode())) {
-        // TODO filter should catch all SEH things, and save the code in the header.
-        std::cerr << "Division by zero happened!" << std::endl;
-        Brigerad::Internals_do_not_use::Details::printException(cpptrace::from_current_exception());
+    CPPTRACE_SEH_EXCEPT(parse_filter(GetExceptionCode())) {
+        printException(cpptrace::from_current_exception());
     }
 }
 
